a861114af71bab39f0c769c7a795369830afa9a3
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-07 18:08:29 -0300

    feat: Add shelter steward on contract deploy

---
+++ contracts/shelter/src/shelter.rs
@@ -0,0 +1,16 @@
+use crate::steward::Steward;
+use soroban_sdk::{contract, contractimpl, Address, Env};
+
+#[contract]
+pub struct Shelter;
+
+#[contractimpl]
+impl Shelter {
+    pub fn __constructor(env: Env, steward: Address) {
+        Steward::new(steward).save_on(&env);
+    }
+
+    pub fn steward(env: Env) -> Address {
+        Steward::from(&env).address()
+    }
+}
---
+++ contracts/shelter/src/storage_types.rs
@@ -0,0 +1,7 @@
+use soroban_sdk::contracttype;
+
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+    Steward,
+}
