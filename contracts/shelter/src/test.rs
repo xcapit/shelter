6666d04c275e385bbc81fe4727b0468e67ab645e
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-10 10:55:33 -0300

    Working...

--- contracts/shelter/src/test.rs
+++ contracts/shelter/src/test.rs
@@ -7,7 +7,6 @@ use soroban_sdk::{
 };
 
 use crate::{
-    storage_types::INSTANCE_BUMP_AMOUNT,
     testtools::{
         assert_auth_fn, assert_instance_ttl_extension, env_with_mock_auths, shelter_id,
         RandomAddresses,
