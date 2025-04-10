0b040a0960745aa8121a1f7234de753ac00b5a1d
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-10 10:35:30 -0300

    feat: Add aid to the shelter

--- contracts/shelter/src/lib.rs
+++ contracts/shelter/src/lib.rs
@@ -1,9 +1,12 @@
 #![no_std]
 
+mod aid;
+mod assigned_aid;
 mod shelter;
 mod steward;
 mod storage_types;
-mod test;
+mod test_shelter_aid;
+mod test_shelter_steward;
 mod testtools;
 
 pub use crate::shelter::ShelterClient;
--- contracts/shelter/src/storage_types.rs
+++ contracts/shelter/src/storage_types.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::contracttype;
+use soroban_sdk::{contracttype, Address};
 
 pub(crate) const DAY_IN_LEDGERS: u32 = 17280;
 pub(crate) const INSTANCE_BUMP_AMOUNT: u32 = 7 * DAY_IN_LEDGERS;
@@ -6,6 +6,21 @@ pub(crate) const INSTANCE_LIFETIME_THRESHOLD: u32 = INSTANCE_BUMP_AMOUNT - DAY_I
 
 #[derive(Clone)]
 #[contracttype]
+pub struct AidDataKey {
+    pub recipient: Address,
+    pub token: Address,
+}
+
+#[contracttype]
+pub struct AidValue {
+    pub amount: i128,
+    pub expiration: i128,
+}
+
+#[derive(Clone)]
+#[contracttype]
 pub enum DataKey {
+    Aid(AidDataKey),
+    AssignedAid(Address),
     Steward,
 }
