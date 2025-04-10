765c628251d855d9bbd0d6b85935834e9e6f0435
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-10 13:51:18 -0300

    Working...

--- contracts/shelter/src/test.rs
+++ contracts/shelter/src/test.rs
@@ -137,7 +137,7 @@ fn test_add_multiple_tokens_aid() {
     let test_amount_2 = 130;
     let env = env_with_mock_auths();
     let [steward, recipient, token_1, token_2, token_3] =
-        RandomAddresses::new(env.clone()).generate::<4>();
+        RandomAddresses::new(env.clone()).generate::<5>();
     let shelter = ShelterClient::new(&env, &shelter_id(&env, &steward));
 
     shelter.add_aid(&recipient, &token, &test_amount);
