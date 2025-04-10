6180141cf125c9b97c0adc66700cd30db7a4f181
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-10 11:16:20 -0300

    Working...

--- contracts/shelter/src/storage_types.rs
+++ contracts/shelter/src/storage_types.rs
@@ -7,8 +7,14 @@ pub(crate) const INSTANCE_LIFETIME_THRESHOLD: u32 = INSTANCE_BUMP_AMOUNT - DAY_I
 #[derive(Clone)]
 #[contracttype]
 pub struct AidDataKey {
-    recipient: Address,
-    token: Address,
+    pub recipient: Address,
+    pub token: Address,
+}
+
+#[contracttype]
+pub struct AidValue {
+    pub amount: i128,
+    pub expiration: i128,
 }
 
 #[derive(Clone)]
