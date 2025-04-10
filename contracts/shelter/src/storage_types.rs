ccf3d926b522bae01b929525ff145edb27b8552c
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-10 11:14:32 -0300

    Working...

--- contracts/shelter/src/storage_types.rs
+++ contracts/shelter/src/storage_types.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::contracttype;
+use soroban_sdk::{contracttype, Address};
 
 pub(crate) const DAY_IN_LEDGERS: u32 = 17280;
 pub(crate) const INSTANCE_BUMP_AMOUNT: u32 = 7 * DAY_IN_LEDGERS;
