d7e110c3f02b7bc0dc030f34e99697189ba1ac7c
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-10 11:58:30 -0300

    Working...

--- contracts/shelter/src/steward.rs
+++ contracts/shelter/src/steward.rs
@@ -38,7 +38,7 @@ impl Steward {
         self.address.clone()
     }
 
-    pub fn dox(&self, action: FnOnce) {
+    pub fn dox(&self, action: fn()) {
         self.address.require_auth();
         action()
     }
