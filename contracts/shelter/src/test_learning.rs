2db92af2f37b87ab56a4f2164803d72b86974579
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-16 11:23:52 -0300

    Working...

--- contracts/shelter/src/test_learning.rs
+++ contracts/shelter/src/test_learning.rs
@@ -8,6 +8,7 @@ use crate::testtools::RandomAddresses;
 #[test]
 fn test_address_and_public_key() {
     let env = Env::default();
+    let keypair = Keypair::generate(&mut thread_rng())
     let [user] = RandomAddresses::new(env.clone()).generate::<1>();
 
     let public_key: BytesN<32> = user.into();
