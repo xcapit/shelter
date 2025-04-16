05989aaa2ff4e841a30a3355775685ddad7e2c48
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-16 11:21:29 -0300

    Working...

--- contracts/shelter/src/test_learning.rs
+++ contracts/shelter/src/test_learning.rs
@@ -3,7 +3,7 @@ extern crate std;
 
 use soroban_sdk::{
     testutils::{Events, MockAuth, MockAuthInvoke},
-    vec, IntoVal, Symbol,
+    vec, Env, IntoVal, Symbol,
 };
 
 use crate::{
@@ -15,23 +15,7 @@ use crate::{
 };
 
 #[test]
-#[should_panic(expected = "Unauthorized function call for address")]
-fn test_bound_aid_unauthorized() {
-    let test_amount = 100;
-    let env = env_with_mock_auths();
-    let [steward, attacker, recipient] = RandomAddresses::new(env.clone()).generate::<3>();
-    let shelter = ShelterClient::new(&env, &shelter_id(&env, &steward));
-    let token = TestToken::new(&env);
-    token.mint(&shelter.address, &test_amount);
-    env.mock_auths(&[MockAuth {
-        address: &attacker,
-        invoke: &MockAuthInvoke {
-            contract: &shelter.address,
-            fn_name: "bound_aid",
-            args: (&recipient, &token.address(), test_amount).into_val(&env),
-            sub_invokes: &[],
-        },
-    }]);
-
-    shelter.bound_aid(&recipient, &token.address(), &test_amount);
+fn test_address_and_public_key() {
+    let env = Env::default();
+    let [user] = RandomAddresses::new(env.clone()).generate::<3>();
 }
