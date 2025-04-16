7d8fe227681ecbd9eaefa245f7950526fb06107e
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-16 11:21:42 -0300

    Working...

--- contracts/shelter/src/test_learning.rs
+++ contracts/shelter/src/test_learning.rs
@@ -1,18 +1,9 @@
 #![cfg(test)]
 extern crate std;
 
-use soroban_sdk::{
-    testutils::{Events, MockAuth, MockAuthInvoke},
-    vec, Env, IntoVal, Symbol,
-};
+use soroban_sdk::Env;
 
-use crate::{
-    testtools::{
-        assert_assigned_aid, assert_auth_fn, assert_instance_ttl_extension, env_with_mock_auths,
-        shelter_id, RandomAddresses, TestToken,
-    },
-    ShelterClient,
-};
+use crate::testtools::RandomAddresses;
 
 #[test]
 fn test_address_and_public_key() {
