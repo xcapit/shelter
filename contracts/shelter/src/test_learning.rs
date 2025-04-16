cfcbcfed67668ffa31e76f670701f6d684b2ac38
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-16 11:41:24 -0300

    Working...

--- contracts/shelter/src/test_learning.rs
+++ contracts/shelter/src/test_learning.rs
@@ -15,6 +15,6 @@ fn test_address_and_public_key() {
 
     assert_eq!(
         user.to_string(),
-        String::from_bytes(&env, keypair.public.to_bytes())
+        String::from_bytes(&env, &keypair.public.to_bytes())
     );
 }
