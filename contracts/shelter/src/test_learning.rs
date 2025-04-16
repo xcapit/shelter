0edba433e5c1bc9ee6d4d40a62091a04600a0ff0
Maxi Nivoli <m_nivoli@hotmail.com>
2025-04-16 11:37:04 -0300

    Working...

--- contracts/shelter/src/test_learning.rs
+++ contracts/shelter/src/test_learning.rs
@@ -3,7 +3,7 @@ extern crate std;
 
 use ed25519_dalek::Keypair;
 use rand::thread_rng;
-use soroban_sdk::{BytesN, Env, String};
+use soroban_sdk::{Address, BytesN, Env, String};
 
 use crate::testtools::RandomAddresses;
 
@@ -12,6 +12,8 @@ fn test_address_and_public_key() {
     let env = Env::default();
     let keypair = Keypair::generate(&mut thread_rng());
     let [user] = RandomAddresses::new(env.clone()).generate::<1>();
+    Address::from_val
+    user.get_
 
     assert_eq!(user.to_string(), keypair.public.to_bytes());
 }
