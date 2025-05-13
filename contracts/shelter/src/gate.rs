6392e0b2ce937905145f2d4cd4f3fe58d7297a66
Maxi Nivoli <m_nivoli@hotmail.com>
2025-05-13 11:54:49 -0300

    feat: Add seal gate to shelter

--- contracts/shelter/src/gate.rs
+++ contracts/shelter/src/gate.rs
@@ -7,19 +7,27 @@ use crate::storage_types::{DataKey, Error};
 pub enum Gate {
     Open,
     Guarded,
+    Sealed,
 }
 
 impl Gate {
     pub fn from(env: &Env) -> Self {
-        env.storage().instance().get(&DataKey::GateState).unwrap()
+        env.storage()
+            .instance()
+            .get(&DataKey::GateState)
+            .unwrap_or(Gate::Open)
     }
 
-    pub fn open() -> Self {
-        Gate::Open
+    pub fn open(&self, env: &Env) {
+        self._to(env, Gate::Open);
     }
 
-    pub fn guard() -> Self {
-        Gate::Guarded
+    pub fn guard(&self, env: &Env) {
+        self._to(env, Gate::Guarded);
+    }
+
+    pub fn seal(&self, env: &Env) {
+        self._to(env, Gate::Sealed)
     }
 
     pub fn expect_perform<F, R>(&self, env: &Env, action: F) -> R
@@ -28,13 +36,15 @@ impl Gate {
     {
         match self {
             Gate::Open => action(),
+            Gate::Sealed => panic_with_error!(env, Error::ShelterSealed),
             Gate::Guarded => panic_with_error!(env, Error::ShelterGuarded),
         }
     }
 
-    pub fn save_on(&self, env: &Env) {
-        env.storage()
-            .instance()
-            .set(&DataKey::GateState, &self.clone());
+    fn _to(&self, env: &Env, state: Gate) {
+        match self {
+            Gate::Sealed => panic_with_error!(env, Error::ShelterSealed),
+            _ => env.storage().instance().set(&DataKey::GateState, &state),
+        }
     }
 }
