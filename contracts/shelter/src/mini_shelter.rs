516ac319e0dd5847e39d74ef85c394d5540c0990
Matias Bruno <matiasbruno97@gmail.com>
2025-05-16 12:25:49 -0300

    Working...

--- contracts/shelter/src/mini_shelter.rs
+++ contracts/shelter/src/mini_shelter.rs
@@ -23,7 +23,7 @@ impl MiniShelter {
     }
 
     pub fn recipient(env: Env) -> BytesN<32> {
-        env.storage().instance().get::<BytesN<32>>(&DataKey::Recipient)
+        env.storage().instance().get::<_, BytesN<32>>(&DataKey::Recipient)
     }
 
     // pub fn init(env: Env, steward_key: BytesN<32>) {
