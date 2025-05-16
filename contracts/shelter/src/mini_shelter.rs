5c72f401dbad166d0e8f17e3eb04228e0b118e8a
Maxi Nivoli <m_nivoli@hotmail.com>
2025-05-16 12:19:47 -0300

    Working...

--- contracts/shelter/src/mini_shelter.rs
+++ contracts/shelter/src/mini_shelter.rs
@@ -1,4 +1,6 @@
-use soroban_sdk::{contract, contractimpl};
+use soroban_sdk::{contract, contractimpl, Address, Env};
+
+use crate::{gate::Gate, steward::Steward};
 
 #[contract]
 pub struct Shelter;
@@ -10,82 +12,82 @@ impl Shelter {
         Gate::from(&env).open(&env);
     }
 
-    pub fn init(env: Env, steward_key: BytesN<32>) {
-        Steward::from(&env).perform(|| {
-            StewardKey::new(steward_key).save_on(&env);
-        });
-        Shelter::_extend_instance_ttl(&env);
-    }
-
-    pub fn open(env: Env) {
-        Steward::from(&env).perform(|| Gate::from(&env).open(&env));
-        Shelter::_extend_instance_ttl(&env);
-    }
-
-    pub fn guard(env: Env) {
-        Steward::from(&env).perform(|| Gate::from(&env).guard(&env));
-        Shelter::_extend_instance_ttl(&env);
-    }
-
-    pub fn seal(env: Env) {
-        Steward::from(&env).perform(|| Gate::from(&env).seal(&env));
-        Shelter::_extend_instance_ttl(&env);
-    }
-
-    pub fn steward_key(env: Env) -> BytesN<32> {
-        StewardKey::from(&env).value()
-    }
-
-    pub fn steward(env: Env) -> Address {
-        Steward::from(&env).address()
-    }
-
-    pub fn update_steward(env: Env, new_steward: Address) {
-        Steward::from(&env).update_on(&env, &new_steward);
-        Shelter::_extend_instance_ttl(&env);
-    }
-
-    pub fn bound_aid(
-        env: Env,
-        recipient: BytesN<32>,
-        token: Address,
-        amount: i128,
-        expiration: u64,
-    ) {
-        Gate::from(&env).expect_perform(&env, || {
-            Steward::from(&env).perform(|| {
-                Aid::from(&env, recipient, token)
-                    .bound(amount, expiration)
-                    .expect_save_on(&env)
-            })
-        });
-        Shelter::_extend_instance_ttl(&env);
-    }
-
-    pub fn unbound_aid(env: Env, recipient: BytesN<32>, token: Address) {
-        Steward::from(&env).perform(|| {
-            Aid::from(&env, recipient, token)
-                .unbound()
-                .expect_save_on(&env);
-        });
-        Shelter::_extend_instance_ttl(&env);
-    }
-
-    pub fn aid_of(env: Env, recipient: BytesN<32>, token: Address) -> AidValue {
-        Aid::from(&env, recipient, token).value()
-    }
-
-    pub fn assigned_aid_of(env: Env, token: Address) -> i128 {
-        AssignedAid::from(&env, token).amount()
-    }
-
-    pub fn available_aid_of(env: Env, token: Address) -> i128 {
-        AvailableAid::from(&env, &token).amount()
-    }
-
-    fn _extend_instance_ttl(env: &Env) {
-        env.storage()
-            .instance()
-            .extend_ttl(INSTANCE_LIFETIME_THRESHOLD, INSTANCE_BUMP_AMOUNT);
-    }
+    // pub fn init(env: Env, steward_key: BytesN<32>) {
+    //     Steward::from(&env).perform(|| {
+    //         StewardKey::new(steward_key).save_on(&env);
+    //     });
+    //     Shelter::_extend_instance_ttl(&env);
+    // }
+    //
+    // pub fn open(env: Env) {
+    //     Steward::from(&env).perform(|| Gate::from(&env).open(&env));
+    //     Shelter::_extend_instance_ttl(&env);
+    // }
+    //
+    // pub fn guard(env: Env) {
+    //     Steward::from(&env).perform(|| Gate::from(&env).guard(&env));
+    //     Shelter::_extend_instance_ttl(&env);
+    // }
+    //
+    // pub fn seal(env: Env) {
+    //     Steward::from(&env).perform(|| Gate::from(&env).seal(&env));
+    //     Shelter::_extend_instance_ttl(&env);
+    // }
+    //
+    // pub fn steward_key(env: Env) -> BytesN<32> {
+    //     StewardKey::from(&env).value()
+    // }
+    //
+    // pub fn steward(env: Env) -> Address {
+    //     Steward::from(&env).address()
+    // }
+    //
+    // pub fn update_steward(env: Env, new_steward: Address) {
+    //     Steward::from(&env).update_on(&env, &new_steward);
+    //     Shelter::_extend_instance_ttl(&env);
+    // }
+    //
+    // pub fn bound_aid(
+    //     env: Env,
+    //     recipient: BytesN<32>,
+    //     token: Address,
+    //     amount: i128,
+    //     expiration: u64,
+    // ) {
+    //     Gate::from(&env).expect_perform(&env, || {
+    //         Steward::from(&env).perform(|| {
+    //             Aid::from(&env, recipient, token)
+    //                 .bound(amount, expiration)
+    //                 .expect_save_on(&env)
+    //         })
+    //     });
+    //     Shelter::_extend_instance_ttl(&env);
+    // }
+    //
+    // pub fn unbound_aid(env: Env, recipient: BytesN<32>, token: Address) {
+    //     Steward::from(&env).perform(|| {
+    //         Aid::from(&env, recipient, token)
+    //             .unbound()
+    //             .expect_save_on(&env);
+    //     });
+    //     Shelter::_extend_instance_ttl(&env);
+    // }
+    //
+    // pub fn aid_of(env: Env, recipient: BytesN<32>, token: Address) -> AidValue {
+    //     Aid::from(&env, recipient, token).value()
+    // }
+    //
+    // pub fn assigned_aid_of(env: Env, token: Address) -> i128 {
+    //     AssignedAid::from(&env, token).amount()
+    // }
+    //
+    // pub fn available_aid_of(env: Env, token: Address) -> i128 {
+    //     AvailableAid::from(&env, &token).amount()
+    // }
+    //
+    // fn _extend_instance_ttl(env: &Env) {
+    //     env.storage()
+    //         .instance()
+    //         .extend_ttl(INSTANCE_LIFETIME_THRESHOLD, INSTANCE_BUMP_AMOUNT);
+    // }
 }
