173a446491c82eb4667a5c24e98166b80b520dc2
Maxi Nivoli <m_nivoli@hotmail.com>
2025-05-21 11:12:02 -0300

    Working...

---
+++ jest.config.e2e.js
@@ -0,0 +1,11 @@
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testEnvironment: "node",
+  testRegex: "./src/tests/e2e/.*\\.(test|spec)?\\.(ts|ts)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  roots: ["<rootDir>/src"],
+};
