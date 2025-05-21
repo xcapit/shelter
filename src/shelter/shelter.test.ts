7e169cf2ad882df9cabfab75b9e41d2c00b81549
Matias Bruno <matiasbruno97@gmail.com>
2025-05-21 14:36:42 -0300

    Working...

--- src/shelter/shelter.test.ts
+++ src/shelter/shelter.test.ts
@@ -1,25 +1,8 @@
 import { Keypair, Networks } from "shelter-sdk";
 import { Shelter } from "./shelter";
 import { FakeRpc } from "../rpc/fake/fake-rpc";
-import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
+import { FakeClient } from "../fake-client/fake-client";
 
-export class FakeClient {
-  constructor(
-    private readonly _options: any,
-    private readonly _steward: Keypair = Keypair.random()
-  ) {}
-  public static deploy() {
-    return {
-      built: { sign: () => {} },
-    };
-  }
-
-  async steward(): Promise<AssembledTransaction<string>> {
-    return Promise.resolve(
-      this._steward.publicKey() as unknown as AssembledTransaction<string>
-    );
-  }
-}
 describe("Shelter", () => {
   const networkPassphrase = Networks.TESTNET;
   const steward = Keypair.random();
