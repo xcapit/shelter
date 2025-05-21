3da7a4ce531760b3e52eff0ca6573abcf4374ef2
Maxi Nivoli <m_nivoli@hotmail.com>
2025-05-21 11:14:09 -0300

    Working...

--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -1,25 +1,12 @@
 
 export default {
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-  },
+  preset: "ts-jest/presets/default-esm",
+  transform: {},
   testEnvironment: "node",
   testRegex: "./src/tests/e2e/.*\\.(test|spec)?\\.(ts|ts)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  roots: ["<rootDir>/src"],
-};
-
-
-export default {
-  preset: "ts-jest/presets/default-esm",
-  testEnvironment: "node",
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["text", "lcov"],
-  testRegex: "./.*\\.(test|spec)\\.ts$",
-  moduleFileExtensions: ["ts", "js", "json"],
-  transform: {},
-  testTimeout: 1000000,
   extensionsToTreatAsEsm: [".ts"],
-  coveragePathIgnorePatterns: ["/node_modules/", "/packages/"],
+  roots: ["<rootDir>/src"],
+  testTimeout: 1000000,
 };
+
