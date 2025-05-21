a38a250b38f71e2f69aa73dd3f44b00aa0ffaeb9
Matias Bruno <matiasbruno97@gmail.com>
2025-05-21 12:01:11 -0300

    Working...

--- src/deployed-shelter/deployed-shelter.ts
+++ src/deployed-shelter/deployed-shelter.ts
@@ -3,7 +3,7 @@ import { Client, Keypair } from "shelter-sdk";
 export class DeployedShelter {
   constructor(
     private readonly _steward: Keypair,
-    private readonly _client: Client
+    private readonly _client: any // TODO: 
   ) {}
 
   stewardId(): Promise<string>{
