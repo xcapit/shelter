8aaedd3bd2025b9be9410055b71c99bf7982867e
Matias Bruno <matiasbruno97@gmail.com>
2025-05-21 11:43:28 -0300

    Working...

--- src/deployed-shelter/deployed-shelter.test.ts
+++ src/deployed-shelter/deployed-shelter.test.ts
@@ -4,9 +4,16 @@ import { DeployedShelter } from "./deployed-shelter";
 describe("DeployedShelter", () => {
   const steward = Keypair.random();
   const client = {} as unknown as Client;
-
+  let deployedShelter: DeployedShelter;
+  beforeAll(()=>{
+    deployedShelter = new DeployedShelter(steward, client)
+  })
   test("new", () => {
     expect(new DeployedShelter(steward, client)).toBeTruthy();
+  });
+
+  test("id", async () => {
+    expect(await deployedShelter.stewardId()).toEqual('id');
   });
 
 });
