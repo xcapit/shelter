45e5533b093ccaea41248917255585a4935c6e01
Matias Bruno <matiasbruno97@gmail.com>
2025-05-21 11:26:00 -0300

    Working...

--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -2,7 +2,8 @@ export default {
   preset: "ts-jest/presets/default-esm",
   transform: {},
   testEnvironment: "node",
-  testRegex: "./src/tests/e2e/.*\\.(test|spec)?\\.(ts|ts)$",
+  testRegex: "./src/tests/e2e/.*\\.e2e\\.test\\.ts$", // 👈 esta es la clave
+  // testRegex: "./src/tests/e2e/.*\\.(test|spec)?\\.(ts|ts)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   extensionsToTreatAsEsm: [".ts"],
   // roots: ["<rootDir>/src"],
