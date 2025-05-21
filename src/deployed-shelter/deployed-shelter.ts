78f723078657855375b114aca090ab13974ac800
Matias Bruno <matiasbruno97@gmail.com>
2025-05-21 14:43:44 -0300

    Working...

--- src/deployed-shelter/deployed-shelter.ts
+++ src/deployed-shelter/deployed-shelter.ts
@@ -5,10 +5,11 @@ import type { FakeClient } from "../fake-client/fake-client";
 export class DeployedShelter {
   constructor(
     private readonly _steward: Keypair,
-    private readonly _client: Client | FakeClient 
+    private readonly _client: Client | FakeClient
   ) {}
 
   async stewardId(): Promise<AssembledTransaction<string>> {
+    console.log("client", this._client);
     return await this._client.steward();
   }
 }
