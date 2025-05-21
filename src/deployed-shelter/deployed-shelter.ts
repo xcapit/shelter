17ad31731280f9311619e267478602bb121aedf2
Matias Bruno <matiasbruno97@gmail.com>
2025-05-21 11:44:04 -0300

    Working...

--- src/deployed-shelter/deployed-shelter.ts
+++ src/deployed-shelter/deployed-shelter.ts
@@ -5,4 +5,8 @@ export class DeployedShelter {
     private readonly _steward: Keypair,
     private readonly _client: Client
   ) {}
+
+  stewardId(): Promise<string>{
+    return Promise.resolve('id');
+  }
 }
