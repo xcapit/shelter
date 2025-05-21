54eab373fa9cbd7aeeaa65d61de198620a22e051
Matias Bruno <matiasbruno97@gmail.com>
2025-05-21 14:36:50 -0300

    Working...

--- src/deployed-shelter/deployed-shelter.ts
+++ src/deployed-shelter/deployed-shelter.ts
@@ -1,11 +1,11 @@
 import { Client, Keypair } from "shelter-sdk";
-import type { FakeClient } from "../shelter/shelter.test";
 import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
+import type { FakeClient } from "../fake-client/fake-client";
 
 export class DeployedShelter {
   constructor(
     private readonly _steward: Keypair,
-    private readonly _client: Client | FakeClient // TODO:
+    private readonly _client: Client | FakeClient 
   ) {}
 
   async stewardId(): Promise<AssembledTransaction<string>> {
