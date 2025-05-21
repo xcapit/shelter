2b92b9521a71d7773c6aecc698fbb7f923e3ca8e
Matias Bruno <matiasbruno97@gmail.com>
2025-05-21 14:37:43 -0300

    Working...

---
+++ src/fake-client/fake-client.test.ts
@@ -0,0 +1,17 @@
+import { Keypair } from "shelter-sdk";
+import { DeployedShelter } from "./deployed-shelter";
+import { FakeClient } from "../fake-client/fake-client";
+
+describe("FakeClient", () => {
+  const steward = Keypair.random();
+
+  test("new", () => {
+    expect(new FakeClient({}, steward)).toBeTruthy();
+  });
+
+  test("steward", async () => {
+    expect(await new FakeClient({}, steward).steward()).toEqual(
+      steward.publicKey()
+    );
+  });
+});
