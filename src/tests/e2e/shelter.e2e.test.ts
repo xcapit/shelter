44744ee4235cad1c897bed6b1fea97b924c248b8
Maxi Nivoli <m_nivoli@hotmail.com>
2025-05-21 11:36:43 -0300

    Working...

--- src/tests/e2e/shelter.e2e.test.ts
+++ src/tests/e2e/shelter.e2e.test.ts
@@ -3,7 +3,7 @@ import { Shelter } from "../../shelter/shelter";
 import { DefaultRpc } from "../../rpc/default/default-rpc";
 
 describe('e2e', () => {
-  test('aoeu', () => {
+  test('aoeu', async () => {
     const steward = Keypair.random();
     const rpc = new DefaultRpc('https://soroban-rpc.testnet.stellar.gateway.fm');
     const shelter = new Shelter(
@@ -12,6 +12,7 @@ describe('e2e', () => {
       '50d8a2d89cb783d34c5400a4548b0335f97c3be58aac7ea3b0f8c4b60b001f4a',
       Networks.TESTNET
     )
-    expect(true).toBeTruthy();
+
+    expect(await shelter.deploy()).toBeTruthy();
   });
 });
