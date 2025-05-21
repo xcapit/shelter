0455041ddd016514acdf6e8ac0fdd27aa9f2152a
Matias Bruno <matiasbruno97@gmail.com>
2025-05-21 11:41:33 -0300

    Working...

--- src/tests/e2e/shelter.e2e.test.ts
+++ src/tests/e2e/shelter.e2e.test.ts
@@ -3,9 +3,7 @@ import { Shelter } from "../../shelter/shelter";
 import { DefaultRpc } from "../../rpc/default/default-rpc";
 import { walletSdk } from "@stellar/typescript-wallet-sdk";
 
-
-describe('e2e', () => {
-
+describe("e2e", () => {
   const _randomKeyPair = async (): Promise<Keypair> => {
     const stellar = walletSdk.Wallet.TestNet().stellar();
     const account = stellar.account().createKeypair();
@@ -13,16 +11,17 @@ describe('e2e', () => {
     return Keypair.fromSecret(account.secretKey);
   };
 
-  
-  test('aoeu', async () => {
-    const steward = Keypair.random();
-    const rpc = new DefaultRpc('https://soroban-rpc.testnet.stellar.gateway.fm');
+  test("aoeu", async () => {
+    const steward = await _randomKeyPair();
+    const rpc = new DefaultRpc(
+      "https://soroban-rpc.testnet.stellar.gateway.fm"
+    );
     const shelter = new Shelter(
       steward,
       rpc,
-      '50d8a2d89cb783d34c5400a4548b0335f97c3be58aac7ea3b0f8c4b60b001f4a',
+      "50d8a2d89cb783d34c5400a4548b0335f97c3be58aac7ea3b0f8c4b60b001f4a",
       Networks.TESTNET
-    )
+    );
 
     expect(await shelter.deploy()).toBeTruthy();
   });
