2430e860a4d52d32c15e421fa0dfbd01edee2d2a
Matias Bruno <matiasbruno97@gmail.com>
2025-05-22 16:49:02 -0300

    Working...

--- src/deployed-shelter/deployed-shelter.test.ts
+++ src/deployed-shelter/deployed-shelter.test.ts
@@ -1,14 +1,16 @@
-import { Keypair } from "shelter-sdk";
+import { Keypair, xdr } from "shelter-sdk";
 import { DeployedShelter } from "./deployed-shelter";
 import { FakeClient } from "../fake-client/fake-client";
 import { FakeRpc } from "../rpc/fake/fake-rpc";
 
 describe("DeployedShelter", () => {
   const steward = Keypair.random();
-  const rpc = new FakeRpc()
+  const rpc = new FakeRpc();
   const client = new FakeClient({}, steward);
   let deployedShelter: DeployedShelter;
-  const recipient = Keypair.fromPublicKey("GASL6XDOK2TO6SCFTXFN2HQDAONLBID2GKX5TYBTHOWA7ZU7VRFZNHGM");
+  const recipient = Keypair.fromPublicKey(
+    "GASL6XDOK2TO6SCFTXFN2HQDAONLBID2GKX5TYBTHOWA7ZU7VRFZNHGM"
+  );
   const tokenContractId =
     "CCQK3OJ5T4A5B4SDKQWH7PQKC5HMUZHIGUWF2INTKDQB32F3YPEW7L27";
   const expiration = BigInt(Math.floor(Date.now() / 1000) + 7200);
@@ -23,10 +25,35 @@ describe("DeployedShelter", () => {
   });
 
   test("stewardId", async () => {
-    expect(await deployedShelter.stewardId()).toEqual(steward.publicKey());
+    const _pollTransactionReponse = {
+      returnValue: {
+        address: (): xdr.ScAddress => {
+          return xdr.ScAddress.scAddressTypeContract(
+            Buffer.from(
+              "227ca7b59a114b96adc361d1d5ce76cb1e2ffe6fa797296749638fded54f9550",
+              "hex"
+            )
+          );
+        },
+      },
+    };
+    expect(
+      await new DeployedShelter(
+        steward,
+        new FakeRpc(_pollTransactionReponse),
+        client
+      ).stewardId()
+    ).toEqual(steward.publicKey());
   });
 
   test("boundAid", async () => {
-    expect(await deployedShelter.boundAid(recipient.rawPublicKey(), tokenContractId, amount, expiration)).toBeTruthy()
-  })
+    expect(
+      await deployedShelter.boundAid(
+        recipient.rawPublicKey(),
+        tokenContractId,
+        amount,
+        expiration
+      )
+    ).toBeTruthy();
+  });
 });
