9a1275e4e3c41e304d5787edc10d7bbc80e7189e
Matias Bruno <matiasbruno97@gmail.com>
2025-05-22 16:49:50 -0300

    Working...

--- src/shelter/shelter.test.ts
+++ src/shelter/shelter.test.ts
@@ -1,4 +1,4 @@
-import { Keypair, Networks } from "shelter-sdk";
+import { Keypair, Networks, xdr } from "shelter-sdk";
 import { Shelter } from "./shelter";
 import { FakeRpc } from "../rpc/fake/fake-rpc";
 import { FakeClient } from "../fake-client/fake-client";
@@ -15,10 +15,22 @@ describe("Shelter", () => {
   });
 
   test("deploy", async () => {
+    const _pollTransactionReponse = {
+      returnValue: {
+        address: (): xdr.ScAddress => {
+          return xdr.ScAddress.scAddressTypeContract(
+            Buffer.from(
+              "227ca7b59a114b96adc361d1d5ce76cb1e2ffe6fa797296749638fded54f9550",
+              "hex"
+            )
+          );
+        },
+      },
+    };
     expect(
       await new Shelter(
         steward,
-        new FakeRpc(),
+        new FakeRpc(_pollTransactionReponse),
         wasmHash,
         networkPassphrase,
         FakeClient
