4425d1d3948d99bb7d2c417447bd3491807caae8
Matias Bruno <matiasbruno97@gmail.com>
2025-05-22 16:45:13 -0300

    Working...

--- src/deployed-shelter/deployed-shelter.ts
+++ src/deployed-shelter/deployed-shelter.ts
@@ -7,7 +7,7 @@ export class DeployedShelter {
     private readonly _steward: Keypair,
     private readonly _rpc: Rpc,
     private readonly _client: Client | FakeClient
-  ) { }
+  ) {}
 
   async stewardId(): Promise<string> {
     return (await this._client.steward()).result;
@@ -19,14 +19,30 @@ export class DeployedShelter {
     amount: bigint,
     expiration: bigint
   ): Promise<boolean> {
-    const tx = (await this._client.bound_aid(
-      {
-        recipient,
-        token,
-        amount,
-        expiration
-      }
-    ));
+    const tx = await this._client.bound_aid({
+      recipient,
+      token,
+      amount,
+      expiration,
+    });
+    const buildTx = tx.built!;
+    buildTx.sign(this._steward);
+    const buildTxResponse = await this._rpc
+      .server()
+      .pollTransaction(
+        (
+          await this._rpc.server().sendTransaction(buildTx)
+        ).hash
+      );
+    //  const boundBuildTx = tx.built!;
+
+    // boundBuildTx.sign(stewardKeypair);
+
+    // const boundTx = await rpcServer.sendTransaction(boundBuildTx);
+    // console.log("[BOUND AID HASH]:", boundTx.hash);
+
+    // const boundTxResponse = await rpcServer.pollTransaction(boundTx.hash);
+    // expect(boundTxResponse.status).toEqual("SUCCESS");
     return true;
   }
 }
