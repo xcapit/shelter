fcd828c7a69f14c1b7da873b3ab89a02f6b25105
Maxi Nivoli <m_nivoli@hotmail.com>
2025-05-22 17:24:58 -0300

    Working...

--- src/tests/e2e/shelter.e2e.test.ts
+++ src/tests/e2e/shelter.e2e.test.ts
@@ -7,6 +7,7 @@ describe("Shelter", async () => {
   const rpc = new DefaultRpc(
     "https://soroban-rpc.testnet.stellar.gateway.fm"
   );
+  const wasmHash = "50d8a2d89cb783d34c5400a4548b0335f97c3be58aac7ea3b0f8c4b60b001f4a",
   const _randomKeyPair = async (): Promise<Keypair> => {
     const stellar = walletSdk.Wallet.TestNet().stellar();
     const account = stellar.account().createKeypair();
