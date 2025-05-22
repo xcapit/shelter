0b1e5ceee1a8bac45934b001d60bf3ac65fb4277
Matias Bruno <matiasbruno97@gmail.com>
2025-05-22 17:03:03 -0300

    Working...

--- src/transaction/transaction.ts
+++ src/transaction/transaction.ts
@@ -1,16 +1,23 @@
-import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
+import type { AssembledTransaction, Tx } from "@stellar/stellar-sdk/contract";
 import type { Keypair } from "shelter-sdk";
 import type { Rpc } from "../rpc/rpc.interface";
 
-export class Transaction{
-    constructor(
-        private readonly _rawTx: AssembledTransaction<any>,
-        private readonly _signer: Keypair,
-        private readonly _rpc: Rpc
-    ){
+export class Transaction {
+  constructor(
+    private readonly _rawTx: AssembledTransaction<any>,
+    private readonly _signer: Keypair,
+    private readonly _rpc: Rpc
+  ) {}
 
-    }
-    async result(){
-        
-    }
+  async result() {
+    const tx = this._rawTx.built!;
+    tx.sign(this._signer);
+    return this._txData(tx);
+  }
+
+  private async _txData(tx: Tx): Promise<any> {
+    return await this._rpc
+      .server()
+      .pollTransaction((await this._rpc.server().sendTransaction(tx)).hash);
+  }
 }
