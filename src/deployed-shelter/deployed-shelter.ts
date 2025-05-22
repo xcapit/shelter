6b2d84592274e70391c7ef283c4da3362d87a22b
Maxi Nivoli <m_nivoli@hotmail.com>
2025-05-22 17:06:08 -0300

    Working...

--- src/deployed-shelter/deployed-shelter.ts
+++ src/deployed-shelter/deployed-shelter.ts
@@ -1,13 +1,14 @@
 import { Client, Keypair } from "shelter-sdk";
 import type { FakeClient } from "../fake-client/fake-client";
 import type { Rpc } from "../rpc/rpc.interface";
+import { Transaction } from "../transaction/transaction";
 
 export class DeployedShelter {
   constructor(
     private readonly _steward: Keypair,
     private readonly _rpc: Rpc,
     private readonly _client: Client | FakeClient
-  ) {}
+  ) { }
 
   async stewardId(): Promise<string> {
     return (await this._client.steward()).result;
@@ -26,17 +27,7 @@ export class DeployedShelter {
       expiration,
     });
     const resultTx = await new Transaction(tx, this._steward, this._rpc).result()
-    
-    const buildTx = tx.built!;
-    buildTx.sign(this._steward);
-    const buildTxResponse = await this._rpc
-      .server()
-      .pollTransaction(
-        (
-          await this._rpc.server().sendTransaction(buildTx)
-        ).hash
-      );
-    if (buildTxResponse.status !== "SUCCESS") {
+    if (resultTx.status !== "SUCCESS") {
       throw new Error("aoeu");
     }
   }
