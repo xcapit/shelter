697675bca9932765453df5599f338872badb6f3f
Matias Bruno <matiasbruno97@gmail.com>
2025-05-22 17:48:54 -0300

    Working...

--- src/tests/e2e/shelter.e2e.test.ts
+++ src/tests/e2e/shelter.e2e.test.ts
@@ -4,8 +4,10 @@ import { DefaultRpc } from "../../rpc/default/default-rpc";
 import { walletSdk } from "@stellar/typescript-wallet-sdk";
 import { Client as SAC } from "sac-sdk";
 
-describe("Shelter", async () => {
-  const defaultRpc = new DefaultRpc("https://soroban-rpc.testnet.stellar.gateway.fm");
+describe("Shelter", () => {
+  const defaultRpc = new DefaultRpc(
+    "https://soroban-rpc.testnet.stellar.gateway.fm"
+  );
   const wasmHash =
     "50d8a2d89cb783d34c5400a4548b0335f97c3be58aac7ea3b0f8c4b60b001f4a";
   const _randomKeyPair = async (): Promise<Keypair> => {
@@ -14,16 +16,29 @@ describe("Shelter", async () => {
     await stellar.fundTestnetAccount(account.publicKey);
     return Keypair.fromSecret(account.secretKey);
   };
-  const steward = await _randomKeyPair();
-  const shelter = new Shelter(steward, defaultRpc, wasmHash, Networks.TESTNET);
 
   test("shelter deploy", async () => {
+    const steward = await _randomKeyPair();
+    const shelter = new Shelter(
+      steward,
+      defaultRpc,
+      wasmHash,
+      Networks.TESTNET
+    );
+
     expect(await (await shelter.deploy()).stewardId()).toEqual(
       steward.publicKey()
     );
   });
 
   test("bound aid", async () => {
+    const steward = await _randomKeyPair();
+    const shelter = new Shelter(
+      steward,
+      defaultRpc,
+      wasmHash,
+      Networks.TESTNET
+    );
     const recipient = Keypair.fromPublicKey(
       "GASL6XDOK2TO6SCFTXFN2HQDAONLBID2GKX5TYBTHOWA7ZU7VRFZNHGM"
     );
@@ -45,10 +60,10 @@ describe("Shelter", async () => {
     const deployedShelter = await shelter.deploy();
 
     const rawMintTx = await _sac.mint({ to: deployedShelter.id(), amount });
-    const builtMintTx = rawMintTx.built!
+    const builtMintTx = rawMintTx.built!;
     builtMintTx.sign(aliceKeyPair);
 
-    const sentMintTx = await defaultRpc.server().sendTransaction(builtMintTx)
+    const sentMintTx = await defaultRpc.server().sendTransaction(builtMintTx);
     const mintTx = await defaultRpc.server().pollTransaction(sentMintTx.hash);
     expect(mintTx.status).toEqual(rpc.Api.GetTransactionStatus.SUCCESS);
 
