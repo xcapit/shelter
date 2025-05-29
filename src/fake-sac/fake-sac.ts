6bccea54bf35b026d20dcd51f504936da30d722e
Matias Bruno <matiasbruno97@gmail.com>
2025-05-29 16:17:31 -0300

    Working...

--- src/fake-sac/fake-sac.ts
+++ src/fake-sac/fake-sac.ts
@@ -1,13 +1,9 @@
+import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
+
 export class FakeSAC {
-  transfer({
-    from,
-    to,
-    amount,
-  }: {
-    from: string;
-    to: string;
-    amount: bigint;
-  }) {
-    
+  transfer({ from, to, amount }: { from: string; to: string; amount: bigint }) {
+    return {
+      built: { sign: () => {} },
+    } as unknown as AssembledTransaction<null>;
   }
 }
