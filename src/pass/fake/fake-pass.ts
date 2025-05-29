be61cc8deec1ebe933eed2324cbea588ae33b45d
Maxi Nivoli <m_nivoli@hotmail.com>
2025-05-29 16:27:41 -0300

    Working...

--- src/pass/fake/fake-pass.ts
+++ src/pass/fake/fake-pass.ts
@@ -1,7 +1,8 @@
+import type { AssembledTransaction } from "@stellar/stellar-sdk/no-axios/contract";
 import type { Keypair } from "shelter-sdk";
 
 export class FakePass {
   constructor(private readonly _recipient: Keypair, private readonly _shelterId: string) { }
 
-  async applyTo() { }
+  async applyTo(tx: AssembledTransaction<null>) { }
 }
