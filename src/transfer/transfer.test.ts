eb3cfde5860eb75eedcf6f60cf40a285705521d4
Matias Bruno <matiasbruno97@gmail.com>
2025-05-29 16:17:52 -0300

    Working...

--- src/transfer/transfer.test.ts
+++ src/transfer/transfer.test.ts
@@ -1,12 +1,13 @@
 import { Keypair, Networks, Client as SAC } from "sac-sdk";
 import { FakeRpc } from "../rpc/fake/fake-rpc";
+import type { FakeSAC } from "../fake-sac/fake-sac";
 
 export class Transfer {
   constructor(
     private readonly _from: string,
     private readonly _to: string,
     private readonly _amount: bigint,
-    private readonly _token: SAC
+    private readonly _token: SAC | FakeSAC
   ) { }
 
   async execute() {
