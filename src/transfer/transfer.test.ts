8a271fd7d8f81df67fda1abf22f73c9d735e8a3c
Maxi Nivoli <m_nivoli@hotmail.com>
2025-05-29 15:56:57 -0300

    Working...

--- src/transfer/transfer.test.ts
+++ src/transfer/transfer.test.ts
@@ -1,6 +1,12 @@
 export class Transfer { }
 
 describe('transfer', () => {
+  const from = '';
+  const to = '';
+  const amount = '';
+  const token = '';
+  const recipient = '';
+
   test('new', () => {
     expect(new Transfer()).toBeTruthy();
   });
