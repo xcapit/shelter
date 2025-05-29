580092801715f02c8ae3dd36185d3697bc2d7989
Matias Bruno <matiasbruno97@gmail.com>
2025-05-29 16:04:31 -0300

    Working...

--- src/transfer/transfer.test.ts
+++ src/transfer/transfer.test.ts
@@ -1,9 +1,11 @@
+import { Keypair, Networks, Client as SAC } from "sac-sdk";
+import { FakeRpc } from "../rpc/fake/fake-rpc";
 export class Transfer {
   constructor(
     private readonly _from: string,
     private readonly _to: string,
     private readonly _amount: bigint,
-    private readonly _token: string // SAC
+    private readonly _token: SAC // SAC
   ) {}
 
   execute() {
@@ -15,8 +17,15 @@ describe("transfer", () => {
   const from = "";
   const to = "";
   const amount = BigInt(1);
-  const token = "";
-  const recipient = "";
+  const tokenContractId = ''
+  const recipient = Keypair.random();
+  const token = new SAC({
+    contractId: tokenContractId,
+    networkPassphrase: Networks.TESTNET,
+    rpcUrl: new FakeRpc().url(),
+    publicKey: recipient.publicKey(),
+  });
+
 
   test("new", () => {
     expect(new Transfer(from, to, amount, token)).toBeTruthy();
