fb8446bb94a96d6be23acd2e0611e484ee730d80
Matias Bruno <matiasbruno97@gmail.com>
2025-05-29 16:18:12 -0300

    Working...

--- src/transfer/transfer.test.ts
+++ src/transfer/transfer.test.ts
@@ -1,5 +1,4 @@
-import { Keypair, Networks, Client as SAC } from "sac-sdk";
-import { FakeRpc } from "../rpc/fake/fake-rpc";
+import { Client as SAC } from "sac-sdk";
 import { FakeSAC } from "../fake-sac/fake-sac";
 
 export class Transfer {
@@ -8,10 +7,14 @@ export class Transfer {
     private readonly _to: string,
     private readonly _amount: bigint,
     private readonly _token: SAC | FakeSAC
-  ) { }
+  ) {}
 
   async execute() {
-    await this._token.transfer({ from: this._from, to: this._to, amount: this._amount });
+    await this._token.transfer({
+      from: this._from,
+      to: this._to,
+      amount: this._amount,
+    });
     return true;
   }
 }
@@ -20,10 +23,7 @@ describe("transfer", () => {
   const from = "CBDH3NC57SPUG2T4HGTR5AIVNEUDIXMEI4SBGZNAS2LPJJMJC7NZWNKF";
   const to = "GASL6XDOK2TO6SCFTXFN2HQDAONLBID2GKX5TYBTHOWA7ZU7VRFZNHGM";
   const amount = BigInt(1);
-  const tokenContractId = 'CCQK3OJ5T4A5B4SDKQWH7PQKC5HMUZHIGUWF2INTKDQB32F3YPEW7L27'
-  const recipient = Keypair.random();
   const token = new FakeSAC();
-
 
   test("new", () => {
     expect(new Transfer(from, to, amount, token)).toBeTruthy();
