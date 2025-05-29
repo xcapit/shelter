dadbde453ff0419e113671b23db3c13a561c76c9
Matias Bruno <matiasbruno97@gmail.com>
2025-05-29 16:32:45 -0300

    Working...

--- src/pass/fake/fake-pass.ts
+++ src/pass/fake/fake-pass.ts
@@ -1,8 +1,7 @@
-import type { AssembledTransaction } from "@stellar/stellar-sdk/no-axios/contract";
 import type { Keypair } from "shelter-sdk";
 
 export class FakePass {
   constructor(private readonly _recipient: Keypair, private readonly _shelterId: string) { }
 
-  async applyTo(tx: AssembledTransaction<null>) { }
+  async applyTo(tx: any) { }
 }
