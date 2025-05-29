8bdb07108e1dd2c9bc561faa19da46a068088585
Matias Bruno <matiasbruno97@gmail.com>
2025-05-29 16:30:56 -0300

    Working...

--- src/fake-sac/fake-sac.ts
+++ src/fake-sac/fake-sac.ts
@@ -1,7 +1,15 @@
 import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
 
 export class FakeSAC {
-  transfer({ from, to, amount }: { from: string; to: string; amount: bigint }) {
+  async transfer({
+    from,
+    to,
+    amount,
+  }: {
+    from: string;
+    to: string;
+    amount: bigint;
+  }): Promise<AssembledTransaction<null>> {
     return {
       built: { sign: () => {} },
     } as unknown as AssembledTransaction<null>;
