ce40d59e00e8fc327b4a4c571c22271c5a3a664d
Matias Bruno <matiasbruno97@gmail.com>
2025-05-30 11:37:57 -0300

    Working...

---
+++ src/transfer/transfer.ts
@@ -0,0 +1,22 @@
+import { Client as SAC } from "sac-sdk";
+import { FakeSAC } from "../fake-sac/fake-sac";
+import type { Pass } from "../pass/pass.interface";
+
+export class Transfer {
+  constructor(
+    private readonly _from: string,
+    private readonly _to: string,
+    private readonly _amount: bigint,
+    private readonly _token: SAC | FakeSAC
+  ) {}
+
+  async value(withPass: Pass) {
+    return await withPass.applyTo(
+      await this._token.transfer({
+        from: this._from,
+        to: this._to,
+        amount: this._amount,
+      })
+    );
+  }
+}
