241268db8e1a444e25cc6bdd1d5f4b0943b7b9a4
Matias Bruno <matiasbruno97@gmail.com>
2025-05-30 11:38:17 -0300

    Working...

--- src/transfer/transfer.test.ts
+++ src/transfer/transfer.test.ts
@@ -2,8 +2,6 @@ import { FakeSAC } from "../fake-sac/fake-sac";
 import { FakePass } from "../pass/fake/fake-pass";
 import { Transfer } from "./transfer";
 
-
-
 describe("transfer", () => {
   const from = "CBDH3NC57SPUG2T4HGTR5AIVNEUDIXMEI4SBGZNAS2LPJJMJC7NZWNKF";
   const to = "GASL6XDOK2TO6SCFTXFN2HQDAONLBID2GKX5TYBTHOWA7ZU7VRFZNHGM";
