34191102996e27e19e0ddc2a18994297c26ac650
Matias Bruno <matiasbruno97@gmail.com>
2025-06-12 17:42:18 -0300

    Working...

--- ltw_service/src/beneficiaries/models/secret-of/secret-of.test.ts
+++ ltw_service/src/beneficiaries/models/secret-of/secret-of.test.ts
@@ -1,7 +1,6 @@
-import { rawBeneficiaryData } from "../../../../fixtures/raw-beneficiary-data";
-import { FakeTwilio } from "../../../../sms/models/fake-twilio/fake-twilio";
-import { Beneficiaries } from "../../beneficiaries/beneficiaries";
-import { FakeBeneficiariesDataRepo } from "../../data-repo/beneficiaries-data-repo/fake/fake-beneficiaries-data-repo";
+import { Beneficiaries } from "../beneficiaries/beneficiaries";
+import { FakeBeneficiariesDataRepo } from "../data-repo/beneficiaries-data-repo/fake/fake-beneficiaries-data-repo";
+import { SecretOf } from "./secret-of";
 
 describe("SecretOf", () => {
   let secretOf: SecretOf;
