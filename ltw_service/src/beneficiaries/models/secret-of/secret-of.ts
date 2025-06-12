c38dd5b36a88f20518fc22e985b896eb99bc973e
Matias Bruno <matiasbruno97@gmail.com>
2025-06-12 17:42:01 -0300

    Working...

--- ltw_service/src/beneficiaries/models/secret-of/secret-of.ts
+++ ltw_service/src/beneficiaries/models/secret-of/secret-of.ts
@@ -1,18 +1,16 @@
 // TODO:
 // import { Hex } from "viem";
-// import { Beneficiaries } from "../../beneficiaries/beneficiaries";
-// import { PrivateKey } from "../private-key.interface";
-//
-// export class PrivateKeyOf implements PrivateKey {
-//
-//   constructor(
-//     private _phoneNumber: string,
-//     private _beneficiaries: Beneficiaries = new Beneficiaries()
-//   ) {}
-//
+import { Beneficiaries } from "../beneficiaries/beneficiaries";
+
+export class SecretOf {
+  constructor(
+    private _phoneNumber: string,
+    private _beneficiaries: Beneficiaries = new Beneficiaries()
+  ) {}
+
 //   async value(): Promise<Hex> {
 //     return (await this._beneficiaries.findPrivateKeyBy(
 //       this._phoneNumber
 //     )) as Hex;
 //   }
-// }
+}
