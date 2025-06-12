18d6d5c53b271b6ed72f6c837ce51c6656a351e4
Maxi Nivoli <m_nivoli@hotmail.com>
2025-06-12 17:30:45 -0300

    Working...

--- ltw_service/src/orders/models/orders/orders.ts
+++ ltw_service/src/orders/models/orders/orders.ts
@@ -1,28 +1,26 @@
-// TODO:
-// import { DefaultOrder } from "../order/default-order/default-order";
-// import {OrderOfReqBody} from "../order-of-req-body/order-of-req-body";
+import { DefaultOrder } from "../order/default-order/default-order";
+import { OrderOfReqBody } from "../order-of-req-body/order-of-req-body";
 import { DefaultBeneficiaryOrdersDataRepo } from "../../../beneficiaries/models/data-repo/beneficiary-orders-data-repo/default/default-beneficiary-orders-data-repo";
 import { BeneficiaryOrdersDataRepo } from "../../../beneficiaries/models/data-repo/beneficiary-orders-data-repo/beneficiary-orders-data-repo.interface";
 import { BeneficiaryOrder } from "../order/beneficiary-order.interface";
-import {NotFoundOrder} from "../order/not-found-order/not-found-order";
+import { NotFoundOrder } from "../order/not-found-order/not-found-order";
 
 export class Orders {
   constructor(
     private _aDataRepo: BeneficiaryOrdersDataRepo = new DefaultBeneficiaryOrdersDataRepo()
-  ) {}
+  ) { }
 
   async findOneByOTPandPhoneNumber(
     anOTP: string,
     aPhoneNumber: string
   ): Promise<BeneficiaryOrder> {
-    // TODO:
-    // const rawBeneficiaryOrder = await this._aDataRepo.findOneByOTPandPhoneNumber(
-    //   anOTP,
-    //   aPhoneNumber
-    // );
-    // return rawBeneficiaryOrder
-    //   ? new DefaultOrder(rawBeneficiaryOrder)
-      // : new NotFoundOrder();
+    const rawBeneficiaryOrder = await this._aDataRepo.findOneByOTPandPhoneNumber(
+      anOTP,
+      aPhoneNumber
+    );
+    return rawBeneficiaryOrder
+      ? new DefaultOrder(rawBeneficiaryOrder)
+      : new NotFoundOrder();
     return new NotFoundOrder()
 
   }
@@ -30,17 +28,16 @@ export class Orders {
   async findOneById(
     anId: string
   ): Promise<BeneficiaryOrder> {
-    // TODO:
-    // const rawBeneficiaryOrder = await this._aDataRepo.findOneById(
-    //   anId
-    // );
-    // return rawBeneficiaryOrder
-    //   ? new DefaultOrder(rawBeneficiaryOrder)
-    //   : new NotFoundOrder();
+    const rawBeneficiaryOrder = await this._aDataRepo.findOneById(
+      anId
+    );
+    return rawBeneficiaryOrder
+      ? new DefaultOrder(rawBeneficiaryOrder)
+      : new NotFoundOrder();
     return new NotFoundOrder()
   }
 
-  async findLatestBy(aPhoneNumber: string, ordersToFetch = 10): Promise<any[]>{
+  async findLatestBy(aPhoneNumber: string, ordersToFetch = 10): Promise<any[]> {
     // TODO:
     // const rawBeneficiaryOrders = await this._aDataRepo.findLatestBy(
     //   aPhoneNumber,
