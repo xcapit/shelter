3acfc654f2ff5acfd88d6296ed6064d2d0ee0a98
Maxi Nivoli <m_nivoli@hotmail.com>
2025-06-12 16:50:19 -0300

    Working...

--- ltw_service/src/beneficiaries/models/transfer/transfer.test.ts
+++ ltw_service/src/beneficiaries/models/transfer/transfer.test.ts
@@ -1,5 +1,4 @@
 import { rawTokenData } from "../../../fixtures/raw-tokens-data";
-import { FakeSmartAccount } from "../smart-account/fake-smart-account/fake-smart-account";
 import { DefaultToken } from "../token/default-token/default-token";
 import { Transfer } from "./transfer";
 
