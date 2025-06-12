84f80a2948f88a56c6b1eff53e8a88201910c617
Matias Bruno <matiasbruno97@gmail.com>
2025-06-12 18:00:01 -0300

    Working...

--- ltw_service/src/orders/orders-server.ts
+++ ltw_service/src/orders/orders-server.ts
@@ -1,170 +1,258 @@
-import { ServerSystem } from "../system/server-system";
-import { Express, NextFunction, Response } from "express";
-import { Beneficiaries } from "../beneficiaries/models/beneficiaries/beneficiaries";
-import { SMS } from "../sms/models/sms/sms";
-import { Tokens } from "../beneficiaries/models/tokens/tokens";
-import { TwilioSMSClient } from "../sms/models/sms-client/twilio-sms-client/twilio-sms-client";
-import { CompleteOrderData, completeOrderSerializer } from "../beneficiaries/serializers/complete-order.serializer";
+import { ServerSystem } from '../system/server-system';
+import { Express, NextFunction, Response } from 'express';
+import { Beneficiaries } from '../beneficiaries/models/beneficiaries/beneficiaries';
+import { SMS } from '../sms/models/sms/sms';
+import { Tokens } from '../beneficiaries/models/tokens/tokens';
+import { TwilioSMSClient } from '../sms/models/sms-client/twilio-sms-client/twilio-sms-client';
+import {
+  CompleteOrderData,
+  completeOrderSerializer,
+} from '../beneficiaries/serializers/complete-order.serializer';
 // TODO
 // import { Orders } from "./models/orders/orders";
 // import {AlchemySmartAccount} from "../beneficiaries/models/smart-account/alchemy-smart-account/alchemy-smart-account";
 // import { PrivateKeyOf } from "../beneficiaries/models/private-key/private-key-of/private-key-of";
 // import { Transfer } from "../beneficiaries/models/transfer/transfer";
-import { OrderOfReqBody } from "./models/order-of-req-body/order-of-req-body";
-import { OrderCompleteMsg } from "./models/messages/order-complete-msg/order-complete-msg";
-import { DefaultOrder } from "./models/order/default-order/default-order";
-import { BalanceOf } from "../shared/balance-of/balance-of";
-import { BeneficiaryOrderResponse } from "./models/beneficiary-order-response/beneficiary-order-response";
-import { AuthorizedRequestOf } from "../system/authorized-request/authorized-request-of";
-import { Users } from "../users/models/users/users";
-import { StatusMsg } from "../metrics/models/status-msg/status-msg";
-import { Limit } from "../system/limit/limit";
-import { OnlyRoles } from "../system/only-roles/only-roles";
-import { Aid, DeployedShelter, Pass, Rpc, SAC, ShelterClient } from "@xcapit/shelter-sdk";
-import { Keypair, rpc as stellarRpc } from "@stellar/stellar-sdk";
+import { OrderOfReqBody } from './models/order-of-req-body/order-of-req-body';
+import { OrderCompleteMsg } from './models/messages/order-complete-msg/order-complete-msg';
+import { DefaultOrder } from './models/order/default-order/default-order';
+import { BalanceOf } from '../shared/balance-of/balance-of';
+import { BeneficiaryOrderResponse } from './models/beneficiary-order-response/beneficiary-order-response';
+import { AuthorizedRequestOf } from '../system/authorized-request/authorized-request-of';
+import { Users } from '../users/models/users/users';
+import { StatusMsg } from '../metrics/models/status-msg/status-msg';
+import { Limit } from '../system/limit/limit';
+import { OnlyRoles } from '../system/only-roles/only-roles';
+import {
+  Aid,
+  DeployedShelter,
+  Pass,
+  Rpc,
+  SAC,
+  ShelterClient,
+} from '@xcapit/shelter-sdk';
+import { Keypair, rpc as stellarRpc } from '@stellar/stellar-sdk';
 import dotenv from 'dotenv';
 import { env } from 'process';
-import { WeiOf } from "../beneficiaries/models/wei-of/wei-of";
-import { SecretOf } from "../beneficiaries/models/secret-of/secret-of";
-import { Orders } from "./models/orders/orders";
+import { WeiOf } from '../beneficiaries/models/wei-of/wei-of';
+import { SecretOf } from '../beneficiaries/models/secret-of/secret-of';
+import { Orders } from './models/orders/orders';
 
 dotenv.config();
 
 export class OrdersServer extends ServerSystem {
-
   constructor(
     private _aServer: Express,
     private _orders: Orders,
     private _beneficiaries: Beneficiaries,
-    private _users: Users
+    private _users: Users,
   ) {
     super();
   }
 
   register() {
-    this._aServer.post('/api/orders', new OnlyRoles(this._users, ['Admin', 'Merchant']).allowedRole(),
-      (req, res, next) => this._createOrder(new AuthorizedRequestOf(req), res, next));
-    this._aServer.post('/api/orders/complete', new OnlyRoles(this._users, ['Admin', 'Merchant']).allowedRole(),
-      (req, res, next) => this._completeOrder(new AuthorizedRequestOf(req), res, next));
-    this._aServer.get('/api/orders/beneficiaries/:phoneNumber', new OnlyRoles(this._users, ['Admin', 'NGO']).allowedRole(),
-      (req, res, next) => this._findLatestOrdersByBeneficiary(new AuthorizedRequestOf(req), res, next));
-    this._aServer.get('/api/orders/:orderId/status', new OnlyRoles(this._users, ['Admin', 'Merchant']).allowedRole(),
-      (req, res, next) => this._getOrderStatus(new AuthorizedRequestOf(req), res, next));
-
+    this._aServer.post(
+      '/api/orders',
+      new OnlyRoles(this._users, ['Admin', 'Merchant']).allowedRole(),
+      (req, res, next) =>
+        this._createOrder(new AuthorizedRequestOf(req), res, next),
+    );
+    this._aServer.post(
+      '/api/orders/complete',
+      new OnlyRoles(this._users, ['Admin', 'Merchant']).allowedRole(),
+      (req, res, next) =>
+        this._completeOrder(new AuthorizedRequestOf(req), res, next),
+    );
+    this._aServer.get(
+      '/api/orders/beneficiaries/:phoneNumber',
+      new OnlyRoles(this._users, ['Admin', 'NGO']).allowedRole(),
+      (req, res, next) =>
+        this._findLatestOrdersByBeneficiary(
+          new AuthorizedRequestOf(req),
+          res,
+          next,
+        ),
+    );
+    this._aServer.get(
+      '/api/orders/:orderId/status',
+      new OnlyRoles(this._users, ['Admin', 'Merchant']).allowedRole(),
+      (req, res, next) =>
+        this._getOrderStatus(new AuthorizedRequestOf(req), res, next),
+    );
   }
 
-  private async _createOrder(req: AuthorizedRequestOf, res: Response, next: NextFunction): Promise<void> {
-    await this.executeAction(async () => {
-      return await (await this._users.findOneBy(req.username())).ifActiveDo<void>(async () => {
-        const order = new OrderOfReqBody(req.body(), this._beneficiaries, new Tokens());
-        const savedOrder = new DefaultOrder(await this._orders.save(order))
+  private async _createOrder(
+    req: AuthorizedRequestOf,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    await this.executeAction(
+      async () => {
+        return await (
+          await this._users.findOneBy(req.username())
+        ).ifActiveDo<void>(async () => {
+          const order = new OrderOfReqBody(
+            req.body(),
+            this._beneficiaries,
+            new Tokens(),
+          );
+          const savedOrder = new DefaultOrder(await this._orders.save(order));
 
-        new SMS(
-          await order.beneficiaryPhoneNumber(),
-          await order.otpMsg(),
-          new TwilioSMSClient()
-        ).send();
+          new SMS(
+            await order.beneficiaryPhoneNumber(),
+            await order.otpMsg(),
+            new TwilioSMSClient(),
+          ).send();
 
-        res.send(new BeneficiaryOrderResponse(savedOrder).toJSON());
-      });
-    }, res, next);
-  }
-
-  private async _completeOrder(req: AuthorizedRequestOf, res: Response, next: NextFunction): Promise<void> {
-    await this.executeAction(async () => {
-      await (await this._users.findOneBy(req.username())).ifActiveDo<void>(async () => {
-        const serializedData: CompleteOrderData =
-          await completeOrderSerializer.validate(req.body());
-
-        const order = await this._orders.findOneByOTPandPhoneNumber(
-          serializedData.otp,
-          serializedData.phoneNumber
-        );
-
-        await this._orders.validateOrder(order);
-
-        let bodyResponse = "Transfer error!";
-        const token = await new Tokens().oneBy(order.tokenSymbol());
-        const beneficiary = await this._beneficiaries.findOneBy(order.phoneNumber());
-        const stewardKeypair = Keypair.fromSecret(env.STEWARD_SECRET!);
-        const rpc = new Rpc(
-          new stellarRpc.Server(env.STELLAR_RPC!)
-        );
-        const sac = new SAC({
-          contractId: token.address(),
-          networkPassphrase: await rpc.network(),
-          rpcUrl: rpc.url(),
-          publicKey: beneficiary.address(),
+          res.send(new BeneficiaryOrderResponse(savedOrder).toJSON());
         });
+      },
+      res,
+      next,
+    );
+  }
 
-        const shelter = new DeployedShelter(
-          stewardKeypair,
-          rpc,
-          new ShelterClient({
-            contractId: env.SHELTER_ID!,
+  private async _completeOrder(
+    req: AuthorizedRequestOf,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    await this.executeAction(
+      async () => {
+        await (
+          await this._users.findOneBy(req.username())
+        ).ifActiveDo<void>(async () => {
+          const serializedData: CompleteOrderData =
+            await completeOrderSerializer.validate(req.body());
+
+          console.log(1);
+          const order = await this._orders.findOneByOTPandPhoneNumber(
+            serializedData.otp,
+            serializedData.phoneNumber,
+          );
+
+          console.log(2);
+          await this._orders.validateOrder(order);
+          console.log(3);
+
+          let bodyResponse = 'Transfer error!';
+          const token = await new Tokens().oneBy(order.tokenSymbol());
+          console.log(4);
+          const beneficiary = await this._beneficiaries.findOneBy(
+            order.phoneNumber(),
+          );
+          console.log(5);
+          const stewardKeypair = Keypair.fromSecret(env.STEWARD_SECRET!);
+          console.log(6);
+          const rpc = new Rpc(new stellarRpc.Server(env.STELLAR_RPC!));
+          console.log(7);
+          const sac = new SAC({
+            contractId: token.address(),
             networkPassphrase: await rpc.network(),
             rpcUrl: rpc.url(),
-            publicKey: stewardKeypair.publicKey(),
-          }),
-        );
+            publicKey: beneficiary.address(),
+          });
+          console.log(8);
 
-        await new Aid(
-          // beneficiary.keypair(),
-          Keypair.fromSecret(await new SecretOf(beneficiary.phoneNumber()).value()),
-          sac,
-          rpc
-        ).transfer(
-          shelter,
-          order.merchAddress(),
-          new WeiOf(order.amount(), token).value(),
-          new Pass(
+          const shelter = new DeployedShelter(
+            stewardKeypair,
+            rpc,
+            new ShelterClient({
+              contractId: env.SHELTER_ID!,
+              networkPassphrase: await rpc.network(),
+              rpcUrl: rpc.url(),
+              publicKey: stewardKeypair.publicKey(),
+            }),
+          );
+          console.log(9);
+
+          await new Aid(
             // beneficiary.keypair(),
-            Keypair.fromSecret(await new SecretOf(beneficiary.phoneNumber()).value()),
-            shelter.id(),
-            rpc
-          )
-        );
+            Keypair.fromSecret(
+              await new SecretOf(beneficiary.phoneNumber()).value(),
+            ),
+            sac,
+            rpc,
+          ).transfer(
+            shelter,
+            order.merchAddress(),
+            new WeiOf(order.amount(), token).value(),
+            new Pass(
+              // beneficiary.keypair(),
+              Keypair.fromSecret(
+                await new SecretOf(beneficiary.phoneNumber()).value(),
+              ),
+              shelter.id(),
+              rpc,
+            ),
+          );
+          console.log(10);
 
-        bodyResponse = new OrderCompleteMsg(
-          await new BalanceOf(
+          bodyResponse = new OrderCompleteMsg(
+            await new BalanceOf(
+              token,
+              beneficiary.keypair(),
+              shelter,
+            ).toAmount(),
             token,
-            beneficiary.keypair(),
-            shelter
-          ).toAmount(),
-          token
-        ).toString();
-        await this._orders.completeOrder(order);
+          ).toString();
+          console.log(11);
+          await this._orders.completeOrder(order);
+          console.log(12);
 
-        new SMS(order.phoneNumber(), bodyResponse, new TwilioSMSClient()).send();
+          new SMS(
+            order.phoneNumber(),
+            bodyResponse,
+            new TwilioSMSClient(),
+          ).send();
 
-        res.send(new StatusMsg("Order Completed!").toJson());
-      });
-    }, res, next);
+          res.send(new StatusMsg('Order Completed!').toJson());
+        });
+      },
+      res,
+      next,
+    );
   }
 
-  private async _findLatestOrdersByBeneficiary(req: AuthorizedRequestOf, res: Response, next: NextFunction): Promise<void> {
-    await this.executeAction(async () => {
-      // TODO:
-      // return await (await this._users.findOneBy(req.username())).ifActiveDo<void>(async () => {
-      //   const orders = await this._orders.findLatestBy(
-      //     (await this._beneficiaries.findOneBy(
-      //       req.params().phoneNumber,
-      //     )).phoneNumber(),
-      //     new Limit(req).toInt()
-      //   );
-      //   res.send(orders.map(order => new BeneficiaryOrderResponse(order).toJSON()));
-      // });
-    }, res, next);
+  private async _findLatestOrdersByBeneficiary(
+    req: AuthorizedRequestOf,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    await this.executeAction(
+      async () => {
+        // TODO:
+        // return await (await this._users.findOneBy(req.username())).ifActiveDo<void>(async () => {
+        //   const orders = await this._orders.findLatestBy(
+        //     (await this._beneficiaries.findOneBy(
+        //       req.params().phoneNumber,
+        //     )).phoneNumber(),
+        //     new Limit(req).toInt()
+        //   );
+        //   res.send(orders.map(order => new BeneficiaryOrderResponse(order).toJSON()));
+        // });
+      },
+      res,
+      next,
+    );
   }
 
-  private async _getOrderStatus(req: AuthorizedRequestOf, res: Response, next: NextFunction): Promise<void> {
-    await this.executeAction(async () => {
-      return await (await this._users.findOneBy(req.username())).ifActiveDo<void>(async () => {
-        const order = await this._orders.findOneById(
-          req.params().orderId
-        );
-        res.send({ status: order.status() });
-      });
-    }, res, next);
+  private async _getOrderStatus(
+    req: AuthorizedRequestOf,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    await this.executeAction(
+      async () => {
+        return await (
+          await this._users.findOneBy(req.username())
+        ).ifActiveDo<void>(async () => {
+          const order = await this._orders.findOneById(req.params().orderId);
+          res.send({ status: order.status() });
+        });
+      },
+      res,
+      next,
+    );
   }
 }
