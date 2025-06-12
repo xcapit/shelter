d6325e7437a8418cfc053b19795d00dd416317cf
Maxi Nivoli <m_nivoli@hotmail.com>
2025-06-12 16:46:28 -0300

    Working...

--- ltw_service/src/orders/models/messages/order-complete-msg/order-complete-msg.ts
+++ ltw_service/src/orders/models/messages/order-complete-msg/order-complete-msg.ts
@@ -1,19 +1,18 @@
-// TODO:
-// import { Token } from '../../../../beneficiaries/models/token/token.interface';
-// import { SmsMsg } from '../../../../shared/messages/sms-msg.interface';
-// import { TranslatedKey } from '../../../../system/multi-language/translated-key/translated-key';
-// import { AmountOf } from '../../../../beneficiaries/models/amount-of/amount-of';
+import { Token } from '../../../../beneficiaries/models/token/token.interface';
+import { SmsMsg } from '../../../../shared/messages/sms-msg.interface';
+import { TranslatedKey } from '../../../../system/multi-language/translated-key/translated-key';
+import { AmountOf } from '../../../../beneficiaries/models/amount-of/amount-of';
 
-// export class OrderCompleteMsg implements SmsMsg {
-//   constructor(
-//     private _anAmountOf: AmountOf,
-//     private _aToken: Token,
-//   ) {}
+export class OrderCompleteMsg implements SmsMsg {
+  constructor(
+    private _anAmountOf: AmountOf,
+    private _aToken: Token,
+  ) { }
 
-//   public toString(): string {
-//     return new TranslatedKey('completeOrderMsg', {
-//       aBalanceAmount: this._anAmountOf.value().toFixed(2),
-//       aToken: this._aToken.symbol(),
-//     }).toString();
-//   }
-// }
+  public toString(): string {
+    return new TranslatedKey('completeOrderMsg', {
+      aBalanceAmount: this._anAmountOf.value().toFixed(2),
+      aToken: this._aToken.symbol(),
+    }).toString();
+  }
+}
