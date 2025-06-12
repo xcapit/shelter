e7c423a3c7fd5b919f465782d28923148c81515c
Maxi Nivoli <m_nivoli@hotmail.com>
2025-06-12 17:34:55 -0300

    Working...

--- ltw_service/src/beneficiaries/models/beneficiary/default-beneficiary/default-beneficiary.ts
+++ ltw_service/src/beneficiaries/models/beneficiary/default-beneficiary/default-beneficiary.ts
@@ -3,10 +3,10 @@ import { Beneficiary } from '../beneficiary.interface';
 import { RawBeneficiary } from '../raw-beneficiary.type';
 
 export class DefaultBeneficiary implements Beneficiary {
-  constructor(private _rawBeneficiaryData: RawBeneficiary) {}
+  constructor(private _rawBeneficiaryData: RawBeneficiary) { }
 
   keypair(): Keypair {
-    return Keypair.fromPublicKey(this.address());
+    return Keypair.fromSecret(this._rawBeneficiaryData.secret);
   }
 
   phoneNumber(): string {
