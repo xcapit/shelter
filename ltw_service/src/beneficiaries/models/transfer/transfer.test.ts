a27b39f12b5a7b8091a49a901ca1a17007177816
Maxi Nivoli <m_nivoli@hotmail.com>
2025-06-12 16:50:10 -0300

    Working...

--- ltw_service/src/beneficiaries/models/transfer/transfer.test.ts
+++ ltw_service/src/beneficiaries/models/transfer/transfer.test.ts
@@ -1,5 +1,4 @@
-// TODO:
-import { rawUSDCData } from "../../../fixtures/raw-tokens-data";
+import { rawTokenData } from "../../../fixtures/raw-tokens-data";
 import { FakeSmartAccount } from "../smart-account/fake-smart-account/fake-smart-account";
 import { DefaultToken } from "../token/default-token/default-token";
 import { Transfer } from "./transfer";
@@ -10,7 +9,7 @@ describe("Transfer", () => {
   beforeEach(() => {
     transfer = new Transfer(
       new FakeSmartAccount(),
-      new DefaultToken(rawUSDCData),
+      new DefaultToken(rawTokenData),
       "10",
       "0xdead"
     );
