f2688559fe9a6c84b9b9fdecc81d8b465b4be536
Maxi Nivoli <m_nivoli@hotmail.com>
2025-06-12 17:44:01 -0300

    Working...

--- ltw_service/src/beneficiaries/models/secret-of/secret-of.ts
+++ ltw_service/src/beneficiaries/models/secret-of/secret-of.ts
@@ -6,11 +6,11 @@ export class SecretOf {
   constructor(
     private _phoneNumber: string,
     private _beneficiaries: Beneficiaries = new Beneficiaries()
-  ) {}
+  ) { }
 
-//   async value(): Promise<Hex> {
-//     return (await this._beneficiaries.findPrivateKeyBy(
-//       this._phoneNumber
-//     )) as Hex;
-//   }
+  async value(): Promise<string> {
+    return (await this._beneficiaries.findPrivateKeyBy(
+      this._phoneNumber
+    ));
+  }
 }
