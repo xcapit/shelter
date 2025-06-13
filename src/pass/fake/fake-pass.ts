8aad540a9618cdbeef407bb2089a0f8f1379c6c8
Matias Bruno <matiasbruno97@gmail.com>
2025-06-13 11:19:39 -0300

    Working...

--- src/pass/fake/fake-pass.ts
+++ src/pass/fake/fake-pass.ts
@@ -1,9 +1,10 @@
 import type { Pass } from "../pass.interface";
+import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
 
 export class FakePass implements Pass {
   constructor() { }
 
-  async applyTo(tx: any) {
+  async applyTo(tx: AssembledTransaction<null>): Promise<AssembledTransaction<null>> {
     return tx;
   }
 }
