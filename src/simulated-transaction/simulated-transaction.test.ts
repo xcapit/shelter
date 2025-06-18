f75af8d071e5d971a148072a179af09b6a84bc13
Matias Bruno <matiasbruno97@gmail.com>
2025-06-18 16:21:22 -0300

    Working...

--- src/simulated-transaction/simulated-transaction.test.ts
+++ src/simulated-transaction/simulated-transaction.test.ts
@@ -1,22 +1,25 @@
-import { Keypair } from "shelter-sdk";
-import { SimulatedTransaction } from "./simulated-transaction";
-import { Rpc } from "../rpc/rpc";
-import { FakeServer } from "../fixtures/fixtures";
 import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
+import { SimulatedTransaction } from "./simulated-transaction";
+import { FakeServer } from "../fixtures/fixtures";
+import { Keypair } from "shelter-sdk";
+import { Rpc } from "../rpc/rpc";
 
-describe('Simulated transaction', () => {
+describe("Simulated transaction", () => {
   const _rpc = {
     assembleTransaction: (raw: any, simulation: any) => {
       return {
-        build: () => ({ sign: () => { } }),
+        build: () => ({ sign: () => {} }),
       };
     },
   };
 
-  test('value', async () => {
-    const simulatedTransaction = new SimulatedTransaction({} as unknown as AssembledTransaction<any>, Keypair.random(), new Rpc(new FakeServer(), _rpc))
-    
-    expect(simulatedTransaction.value()).toBeTruthy()
-  });
+  test("value", async () => {
+    const simulatedTransaction = new SimulatedTransaction(
+      {} as unknown as AssembledTransaction<any>,
+      Keypair.random(),
+      new Rpc(new FakeServer(), _rpc)
+    );
 
+    expect(simulatedTransaction.value()).toBeTruthy();
+  });
 });
