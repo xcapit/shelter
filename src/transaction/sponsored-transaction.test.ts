1867ab414c69d22cb799f7f91c1f25c015c1f56a
francisco.arduh <francisco.arduh@gmail.com>
2025-06-18 12:32:43 -0300

    Working...

--- src/transaction/sponsored-transaction.test.ts
+++ src/transaction/sponsored-transaction.test.ts
@@ -4,36 +4,45 @@ import { Rpc } from "../rpc/rpc";
 import { FakeServer } from "../fixtures/fixtures";
 import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
 
-export class SponsoredTransaction{
-      constructor(
-    private readonly _innerTx : any,
+export class SponsoredTransaction {
+  constructor(
+    private readonly _innerTx: any,
     private readonly _signer: Keypair,
     private readonly _rpc: Rpc
-  ) { }
+  ) {}
 
-
+  async result(): Promise<rpc.Api.GetTransactionResponse> {
+    return await this._txData(await this.value());
+  }
 }
 
-describe('Simulated transaction', () => {
-    const sponsor = Keypair.random();
-    const _rpc = {
-        assembleTransaction: (raw: any, simulation: any) => {
-        return {
-            build: () => ({ sign: () => { } }),
-        };
-        },
-    };
+describe("Simulated transaction", () => {
+  const sponsor = Keypair.random();
+  const _rpc = {
+    assembleTransaction: (raw: any, simulation: any) => {
+      return {
+        build: () => ({ sign: () => {} }),
+      };
+    },
+  };
 
-  test('new', async () => {
-    const sponsoredTransaction = new SponsoredTransaction({} as unknown as AssembledTransaction<any>, sponsor, new Rpc(new FakeServer(), _rpc));
-    
-    expect(sponsoredTransaction).toBeTruthy()
+  test("new", async () => {
+    const sponsoredTransaction = new SponsoredTransaction(
+      {} as unknown as AssembledTransaction<any>,
+      sponsor,
+      new Rpc(new FakeServer(), _rpc)
+    );
+
+    expect(sponsoredTransaction).toBeTruthy();
   });
 
- test('result', async () => {
-    const sponsoredTransaction = new SponsoredTransaction({} as unknown as AssembledTransaction<any>, sponsor, new Rpc(new FakeServer(), _rpc));
-    
-    expect(sponsoredTransaction.result()).toBeTruthy()
-  });
+  test("result", async () => {
+    const sponsoredTransaction = new SponsoredTransaction(
+      {} as unknown as AssembledTransaction<any>,
+      sponsor,
+      new Rpc(new FakeServer(), _rpc)
+    );
 
+    expect(sponsoredTransaction.result()).toBeTruthy();
+  });
 });
