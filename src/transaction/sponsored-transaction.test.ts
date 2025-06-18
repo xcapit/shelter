9b6932857119318bab322259952c2ed8042d8ac4
francisco.arduh <francisco.arduh@gmail.com>
2025-06-18 11:47:25 -0300

    Working...

---
+++ src/transaction/sponsored-transaction.test.ts
@@ -0,0 +1,23 @@
+import { Keypair } from "shelter-sdk";
+import { SimulatedTransaction } from "./simulated-transaction";
+import { Rpc } from "../rpc/rpc";
+import { FakeServer } from "../fixtures/fixtures";
+import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
+
+describe('Simulated transaction', () => {
+  
+    const _rpc = {
+        assembleTransaction: (raw: any, simulation: any) => {
+        return {
+            build: () => ({ sign: () => { } }),
+        };
+        },
+    };
+
+  test('new', async () => {
+    const sponsoredTransaction = new SponsoredTransaction({} as unknown as AssembledTransaction<any>, Keypair.random(), new Rpc(new FakeServer(), _rpc));
+    
+    expect(simulatedTransaction.value()).toBeTruthy()
+  });
+
+});
