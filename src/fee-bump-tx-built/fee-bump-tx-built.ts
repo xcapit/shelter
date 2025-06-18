3b4d8f3a22ff4109de11d731ac888752dbff3cd5
Matias Bruno <matiasbruno97@gmail.com>
2025-06-18 16:20:34 -0300

    Working...

---
+++ src/fee-bump-tx-built/fee-bump-tx-built.ts
@@ -0,0 +1,15 @@
+import * as StellarSDK from "@stellar/stellar-sdk";
+import { Keypair } from "shelter-sdk";
+
+export class FeeBumpTxBuilt {
+  constructor(private readonly _txBuilder = StellarSDK.TransactionBuilder) {}
+
+  value(signer: Keypair, fee: string, innerTx: any, networkPassphrase: string) {
+    return this._txBuilder.buildFeeBumpTransaction(
+      signer,
+      fee,
+      innerTx,
+      networkPassphrase
+    );
+  }
+}
