085dd3869c358c448127a9fcc417d8896d71159e
Matias Bruno <matiasbruno97@gmail.com>
2025-06-18 16:27:30 -0300

    Working...

--- src/aid/aid.ts
+++ src/aid/aid.ts
@@ -4,8 +4,8 @@ import { DeployedShelter } from "../deployed-shelter/deployed-shelter";
 import { Rpc } from "../rpc/rpc";
 import type { Pass } from "../pass/pass.interface";
 import { Transfer } from "../transfer/transfer";
-import { SimulatedTransaction } from "../transaction/simulated-transaction";
 import { FakeSAC } from "../fake-sac/fake-sac";
+import { SimulatedTransaction } from "../simulated-transaction/simulated-transaction";
 
 export class Aid {
   constructor(
