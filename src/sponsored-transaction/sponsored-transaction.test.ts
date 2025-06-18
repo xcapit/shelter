8efa7d2553d104351816bf32804c8127b64af1f2
Matias Bruno <matiasbruno97@gmail.com>
2025-06-18 16:21:02 -0300

    Working...

--- src/sponsored-transaction/sponsored-transaction.test.ts
+++ src/sponsored-transaction/sponsored-transaction.test.ts
@@ -1,48 +1,9 @@
-import { Keypair, rpc as StellarRpc } from "shelter-sdk";
-import { Rpc } from "../rpc/rpc";
-import { FakeServer } from "../fixtures/fixtures";
 import type { AssembledTransaction } from "@stellar/stellar-sdk/contract";
-import * as StellarSDK from "@stellar/stellar-sdk";
-
-class FeeBumpTxBuilt {
-  constructor(private readonly _txBuilder = StellarSDK.TransactionBuilder) {}
-
-  value(signer: Keypair, fee: string, innerTx: any, networkPassphrase: string) {
-    return this._txBuilder.buildFeeBumpTransaction(
-      signer,
-      fee,
-      innerTx,
-      networkPassphrase
-    );
-  }
-}
-export class SponsoredTransaction {
-  constructor(
-    private readonly _innerTx: any,
-    private readonly _signer: Keypair,
-    private readonly _rpc: Rpc,
-    private readonly _feeBumpTxBuilt: FeeBumpTxBuilt
-  ) {}
-
-  async result(): Promise<StellarRpc.Api.GetTransactionResponse | void> {
-    const feeBumpTransaction = this._feeBumpTxBuilt.value(
-      this._signer,
-      (+StellarSDK.BASE_FEE * 2).toString(),
-      this._innerTx,
-      await this._rpc.network()
-    );
-    feeBumpTransaction.sign(this._signer);
-    return await this._txData(feeBumpTransaction);
-  }
-
-  private async _txData(
-    tx: StellarSDK.FeeBumpTransaction
-  ): Promise<StellarRpc.Api.GetTransactionResponse> {
-    return await this._rpc
-      .server()
-      .pollTransaction((await this._rpc.server().sendTransaction(tx)).hash);
-  }
-}
+import { FeeBumpTxBuilt } from "../fee-bump-tx-built/fee-bump-tx-built";
+import { SponsoredTransaction } from "./sponsored-transaction";
+import { FakeServer } from "../fixtures/fixtures";
+import { Keypair } from "shelter-sdk";
+import { Rpc } from "../rpc/rpc";
 
 describe("Sponsored transaction", () => {
   const sponsor = Keypair.random();
